99e4691099d996855ee1ab44ff412f4762ac2a65
Gary Kerr <garykerrwork@gmail.com>
2020-10-23 22:27:19 +0100

    Initial Commit

---
+++ GuessWho/GuessWhoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessWhoApp.swift
+//  GuessWho
+//
+//  Created by gary on 23/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct GuessWhoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
